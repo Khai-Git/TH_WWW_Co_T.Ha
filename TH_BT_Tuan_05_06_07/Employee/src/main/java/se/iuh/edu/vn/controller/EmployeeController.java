d3ed9075c6814857b3b7ccc41bb8cc3b0a582637
Khai-Git <kelvinnon28092001@gmail.com>
2023-11-05 08:29:09 +0700

    Cái thư mục sping-hibernate-onetomany-security-demo là của bn Vinh, An coi thử, t thì hiểu r đó nhưng nó có 2 entity còn bài của cô Hà là 1 entity nên t chưa code đc

--- TH_BT_Tuan_05_06_07/Employee/src/main/java/se/iuh/edu/vn/controller/EmployeeController.java
+++ TH_BT_Tuan_05_06_07/Employee/src/main/java/se/iuh/edu/vn/controller/EmployeeController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,48 +13,48 @@ import org.springframework.web.servlet.ModelAndView;
 
 import se.iuh.edu.vn.beans.Employee;
 import se.iuh.edu.vn.dao.EmployeeDAO;
+import se.iuh.edu.vn.dao.EmployeeService;
 
 @Controller
-
 public class EmployeeController {
 	@Autowired
-	EmployeeDAO dao;
-	
-	@RequestMapping("/empform")
-	public ModelAndView showform() {
-		return new ModelAndView("empform", "command", new EmployeeDAO());
-	}
+	EmployeeService service;
 
-	@RequestMapping(value = "/save", method = RequestMethod.POST)
-	public ModelAndView save(@ModelAttribute("emp") Employee emp) {
-		dao.save(emp);
-		return new ModelAndView("redirect:/viewemp");
-	}
-	
+//	@RequestMapping("/empform")
+//	public ModelAndView showform() {
+//		return new ModelAndView("empform", "command", new EmployeeDAO());
+//	}
+//
+//	@RequestMapping(value = "/save", method = RequestMethod.POST)
+//	public ModelAndView save(@ModelAttribute("emp") Employee emp) {
+//		dao.save(emp);
+//		return new ModelAndView("redirect:/viewemp");
+//	}
+//	
 	@RequestMapping("/viewemp")
 	public String viewemp(Model model) {
-		List<Employee> list = dao.getEmployees();
-		model.addAttribute("list",list);
+		List<Employee> list = service.getList();
+		model.addAttribute("list", list);
 //		return new ModelAndView("list", "viewemp", list);
 		return "viewemp";
 	}
 
-	@RequestMapping(value = "/editemp/{id}")
-	public ModelAndView edit(@PathVariable int id) {
-		Employee emp = dao.getEmpById(id);
-		return new ModelAndView("empeditform", "command", emp);
-	}
-
-	/* Cập nhật đối tượng model. */
-	@RequestMapping(value = "/editsave", method = RequestMethod.POST)
-	public ModelAndView editsave(@ModelAttribute("emp") Employee emp) {
-		dao.update(emp);
-		return new ModelAndView("redirect:/viewemp");
-	}
-
-	@RequestMapping(value = "/deleteemp/{id}", method = RequestMethod.GET)
-	public ModelAndView delete(@PathVariable int id) {
-		dao.delete(id);
-		return new ModelAndView("redirect:/viewemp");
-	}
+//	@RequestMapping(value = "/editemp/{id}")
+//	public ModelAndView edit(@PathVariable int id) {
+//		Employee emp = dao.getEmpById(id);
+//		return new ModelAndView("empeditform", "command", emp);
+//	}
+//
+//	/* Cập nhật đối tượng model. */
+//	@RequestMapping(value = "/editsave", method = RequestMethod.POST)
+//	public ModelAndView editsave(@ModelAttribute("emp") Employee emp) {
+//		dao.update(emp);
+//		return new ModelAndView("redirect:/viewemp");
+//	}
+//
+//	@RequestMapping(value = "/deleteemp/{id}", method = RequestMethod.GET)
+//	public ModelAndView delete(@PathVariable int id) {
+//		dao.delete(id);
+//		return new ModelAndView("redirect:/viewemp");
+//	}
 }
